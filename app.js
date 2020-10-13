942a8634e7e66fa014930d1863603f7598a30bc9
karthiksista <karthik.sista6@gmail.com>
2020-10-13 08:40:56 +0530

    first beat

---
+++ app.js
@@ -0,0 +1,25 @@
+function sequencer () {
+ const kick = new Tone.Player('./kick-electro01.wav').toDestination();
+ const snare = new Tone.Player('./clap-808.wav').toDestination();
+ let index =0; 
+
+ Tone.Transport.scheduleRepeat(repeat,'8n');
+ Tone.Transport.start();
+ function repeat() {
+     let step = index % 8;
+     let kickInputs = document.querySelector(`.kick input:nth-child(${step +1})`);
+     let snareInputs = document.querySelector(`.snare input:nth-child(${step +1})`);
+     if(kickInputs.checked) {
+         kick.start();
+     }
+     if(snareInputs.checked) {
+        snare.start();
+    }
+     index++;
+ }
+}
+
+
+sequencer();
+
+
